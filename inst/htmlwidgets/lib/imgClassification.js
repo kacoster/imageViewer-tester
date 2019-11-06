f7a0cb63c5334ecb6c62916976c434cca6953f66
dmdeveloper <kudatawira@gmail.com>
2019-11-06 13:37:14 +0200

    add slider input

--- inst/htmlwidgets/lib/imgClassification.js
+++ inst/htmlwidgets/lib/imgClassification.js
@@ -118,7 +118,7 @@
         ************************************************************************/
 
       // Read the batch Image Number from from slider
-      Shiny.addCustomMessageHandler("batchImageSizeMessage",
+      Shiny.addCustomMessageHandler("img_clssfctn_ud_btch_img_thrshld",
         function(message) {
             imgNumb =  parseInt(JSON.stringify(message));
             console.log("Image Number from the Slider : " + imgNumb);
