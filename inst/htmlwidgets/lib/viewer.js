ab5569d56b997135da9062d7814a9aef9aa1e5ee
dmdeveloper <kudatawira@gmail.com>
2019-11-12 14:47:57 +0200

    Update viewer.js

--- inst/htmlwidgets/lib/viewer.js
+++ inst/htmlwidgets/lib/viewer.js
@@ -1845,8 +1845,6 @@
           if(getSelectedImages().includes(url) || removedRef() === url)
           {
             console.log("Checking URL  : " + url);
-            getCurrClckdImg("clssfctn_slctd_img",
-                url.substring(url.lastIndexOf("/") + 1, url.length ));
 
             console.log("Um hidding the clicked image");
             this.image = image;
