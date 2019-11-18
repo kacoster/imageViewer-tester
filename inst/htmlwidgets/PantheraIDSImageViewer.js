3e3aaba7772ef5308bccecc746012ce212320bd7
dmdeveloper <kudatawira@gmail.com>
2019-11-18 14:11:05 +0200

    object class

--- inst/htmlwidgets/PantheraIDSImageViewer.js
+++ inst/htmlwidgets/PantheraIDSImageViewer.js
@@ -16,30 +16,37 @@ HTMLWidgets.widget({
 
     return {
 
-      renderValue: function(x) {
+    renderValue: function(x) {
 
-        /**
-         * Calling readServerData function
-         * Passing the filePath parameter as an argument
-        */
+      /**
+       * Calling readServerData function
+       * Passing the filePath parameter as an argument
+      */
 
-        if(x.componentID === "img_clssfctn_ud")
-        {
-          console.log("CASE : classification module ");
-          processClsfctnResponseText(x.message);
 
-        }
-        else if (x.componentID === "spcs_idntfctn_id_rf_1")
-        {
-          console.log("CASE : spcs_idntfctn_id_rf_1 ");
-          processIdnfctn1ResponseText(x.message);
-        }
-        else
-        {
-          processIdnfctn2ResponseText(x.message);
-        }
+      if(x.componentID === "img_clssfctn_ud")
+      {
+        let classification = new createViewer('imgsArray','selected_images',1,50,x.componentID);
+        classification.processClsfctnResponseText(x.message);
+        console.log("CASE : classification module ");
+        //processClsfctnResponseText(x.message);
+      }
+      else if (x.componentID === "spcs_idntfctn_id_rf_1")
+      {
+        let spcsIdntfctn1 = new createViewer('imgsArray','selected_images',1,20,x.componentID);
+        spcsIdntfctn1.processClsfctnResponseText(x.message);
+        console.log("CASE : spcs_idntfctn_id_rf_1 ");
+        //processIdnfctn1ResponseText(x.message);
+      }
+      else
+      {
+        let spcsIdntfctn2 = new createViewer('imgsArray','selected_images',2,20,x.componentID);
+        spcsIdntfctn2.processClsfctnResponseText(x.message);
+        console.log("CASE : spcs_idntfctn_id_rf_1 ");
+        //processIdnfctn2ResponseText(x.message);
+      }
 
-      },
+    },
 
       resize: function(width, height) {
 
---
+++ inst/htmlwidgets/lib/createViewer.js
@@ -0,0 +1,225 @@
+    var createViewer = function(imgsArray,selected_images,batnum,imgNumb,viewerId){
+
+        this.imgsArray = imgsArray;
+        this.selected_images = selected_images;
+        this.batnum = batnum;
+        this.imgNumb = imgNumb;
+        this.viewerId = viewerId;
+    }
+
+    createViewer.prototype.getBatchNumber = function () {
+        console.log(`${this.imgNumb} elapsed batches `);
+        return this.imgsArray;
+    };
+
+    createViewer.prototype.fetchServerFile = async function (msg) {
+        let result= await (await fetch(msg)).text();
+        return result.replace(/^\s*$[\n\r]{1,}/gm, '');
+    };
+
+    createViewer.prototype.processResponseText = async function (csvfile) {
+
+    let textResult = await this.fetchServerFile(csvfile);
+    this.imgsArray = (textResult).split(',');
+    this.imgsArray.splice(0, 1);
+    this.imgsArray[0] =  this.imgsArray[0].replace("Source", "");
+    this.imgsArray[0] =  this.imgsArray[ this.imgsArray.length - 1] + this.imgsArray[0];
+    this.imgsArray.splice( this.imgsArray.length - 1, 1);
+    console.log(this.imgsArray);
+    this.displayImages(0);
+
+    // Read the batch Image Number from from slider : img_clssfctn_ud_btch_img_thrshld
+    /*Shiny.addCustomMessageHandler("img_clssfctn_ud_batch_image_size",
+    function(message) {
+        this.imgNumb =  parseInt(JSON.stringify(message));
+            Shiny.onInputChange("img_clssfctn_ud_btch_tckr",
+            1 + " / " + this.getBatchNumber());
+            this.displayImages(0);
+        }
+    );*/
+
+    };
+
+    createViewer.prototype.handleExistance = function (params,src,id)
+    {
+      console.log("Line : " + 181);
+      if(params.includes(src))
+      {
+        tempRemoved =  (params.splice(params.indexOf(src),1))[0];
+        this.removeHighlight(id);
+        if(params.length > 0)
+        {
+          this.getCurrClckdImg("clssfctn_slctd_img",
+          params[params.length -1].substring(
+            src.lastIndexOf("/") + 1, src.length ));
+        }else{
+          getCurrClckdImg("clssfctn_slctd_img","");
+        }
+      }
+      else{
+        console.log("Line : " + 188);
+        params.push(src);
+        $(".pictures > li").css("background-color", "yellow");
+         this.highliter(id);
+         this.getCurrClckdImg("clssfctn_slctd_img",
+            src.substring(src.lastIndexOf("/") + 1, src.length ));
+      }
+    };
+
+    createViewer.prototype.highliter = function (imgId)
+    {
+        console.log("Highting the Image");
+        $('#' + imgId + '').css({
+              'opacity': '0.4',
+              'filter': 'alpha(opacity=40)'
+            });
+    };
+
+    createViewer.prototype.removeHighlight = function (imgId)
+    {
+        $('#' + imgId + '').css({
+              'opacity': '',
+              'filter': ''
+            });
+    };
+
+    createViewer.prototype.getCurrClckdImg = function (state, imgsrc)
+    {
+        Shiny.onInputChange(state,imgsrc);
+    };
+
+    createViewer.prototype.isKeyPressed = function (event) {
+
+        this.arrayClone(selected_images);
+
+        if (event.shiftKey) {
+            this.handleExistance(this.selected_images, event.target.src, event.target.id);
+
+        } else {
+             objectOf("imgClassification");
+             this.viewerComponent(this.viewerId);
+          }
+    };
+
+    // createViewerComponent_rf2 , vjs() , createViewerComponent
+    createViewer.prototype.viewerComponent = function () {
+        var viewer = new Viewer(document.getElementById(this.galid), {
+          url: 'data-original',
+          title: function (image) {
+            return image.alt + ' (' + (this.index + 1) + '/' + this.length + ')';
+          },
+        });
+      };
+
+    createViewer.prototype.arrayClone =  function (param){
+        selected_images_clone = [...param];
+        //return selected_images_clone;
+     };
+
+    createViewer.prototype.objectOf = function (viewerType)
+    {
+      return viewerType;
+    };
+
+    createViewer.prototype.nextPrevClicked = function (status) // ATTENTION
+    {
+        nextPrev = status;
+    };
+
+    createViewer.prototype.getCurrClckdImg = function (state, imgsrc) // ATTENTION
+    {
+      Shiny.onInputChange(state,imgsrc);
+    };
+
+    function clickEventStatus(status)
+    {
+      clickStatus = status;
+    };
+
+    createViewer.prototype.removedRef = function  ()
+    {
+        return tempRemoved;
+    };
+
+    // imgloop
+    createViewer.prototype.buildImages = function (arry) {
+        $(".pictures > li").css("background-color", "white");
+        for (i = 0; i < arry.length; i++) {
+          let liId = i;
+          let img = new Image();
+          let ul = document.getElementById(this.viewerId);
+          img.src = ((arry[i].trim()).replace(/['"]+/g, '')).replace(/(\r\n|\n|\r)/gm,"");
+          img.alt = "Camera Trap";
+          img.datamarked = 0;
+          ul.innerHTML += '<li ><img id="' + liId + '" data-original="' +
+          img.src + '"  marked="' + img.datamarked + '" src="' +
+          img.src + '" alt="' + img.alt + '" /> </li>';
+        }
+    };
+
+    createViewer.prototype.displayImages = function (bat) {
+        let startIndex , endIndex;
+        this.clearImages();
+        startIndex = bat * this.imgnumb;
+        endIndex = startIndex + imgnumb;
+        resultsArray = this.imgsArray.slice(startIndex, endIndex);
+        this.buildImages(resultsArray);
+    };
+
+    createViewer.prototype.clearImages = function () {
+        //console.log("In clearImages()");
+        $('#' + this.viewerId + '').html("");
+        //$("#x").html("");
+    };
+
+    createViewer.prototype.getBatchNumber = function () {
+
+        if((this.imgsArray.length %  this.imgNumb)===0){
+            return (this.imgsArray.length / this.imgNumb);
+        }
+        else{
+          return ((Math.floor(this.imgsArray.length / this.imgNumb)) + 1);
+        }
+    };
+
+    createViewer.prototype.next = function () {
+        this.nextPrevClicked("1");
+
+        if(this.batnum < this.getBatchNumber()-1){
+            this.batnum++;
+               //console.log("Tester : " +   batnum + "/" + getBatchNumber());
+            Shiny.onInputChange("img_clssfctn_ud_btch_tckr",
+                (this.batnum + 1) + " / " + this.getBatchNumber());
+            this.clearImages();
+            this.displayImages(this.batnum);
+
+        }else{
+           Shiny.onInputChange("img_clssfctn_ud_btch_tckr",
+             this.getBatchNumber() + " / " + this.getBatchNumber());
+             this.clearImages();
+             this.displayImages(this.getBatchNumber()-1);
+             this.batnum = this.getBatchNumber()-1;
+        }
+    };
+
+    createViewer.prototype.prev = function () {
+        this.nextPrevClicked("1");
+        this.batnum--;
+        if (this.batnum > 0 ) {
+           Shiny.onInputChange("img_clssfctn_ud_btch_tckr",
+              (this.batnum+1) + " / " + this.getBatchNumber());
+              this.clearImages();
+              this.displayImages(this.batnum);
+        }else{
+           Shiny.onInputChange("img_clssfctn_ud_btch_tckr",
+             1 + " / " + this.getBatchNumber());
+             this.clearImages();
+             this.displayImages(0);
+             this.batnum = 0;
+        }
+    };
+
+    //let classification = new createViewer('imgsArray','selected_images',1,50);
+    //console.log(classification.getBatchNumber());
+    //classification.processResponseText('img_clssfctn_ud.csv');
+   // console.log(" Checking my Car : "+ car);
