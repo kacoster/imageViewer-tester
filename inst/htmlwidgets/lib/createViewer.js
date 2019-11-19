0d71a65eed00374735a88d2fecbd03d34cf0a245
dmdeveloper <kudatawira@gmail.com>
2019-11-19 10:50:40 +0200

    Update createViewer.js

--- inst/htmlwidgets/lib/createViewer.js
+++ inst/htmlwidgets/lib/createViewer.js
@@ -81,7 +81,8 @@
     // Read the batch Image Number from from slider : img_clssfctn_ud_btch_img_thrshld
     Shiny.addCustomMessageHandler("img_clssfctn_ud_batch_image_size",
     function(message) {
-        this.imgNumber =  parseInt(JSON.stringify(message));
+            this.imgNumber =  parseInt(JSON.stringify(message));
+            console.log("Image Number In SHiny.adCustMH : " + this.imgNumber);
             Shiny.onInputChange("img_clssfctn_ud_btch_tckr",
             1 + " / " + this.getBatchNumber());
             this.displayImages(this.batnum);
