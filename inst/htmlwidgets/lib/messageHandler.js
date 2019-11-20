bc220df1e18cb594d826d6c5c247c60ba5888816
dmdeveloper <kudatawira@gmail.com>
2019-11-20 09:41:47 +0200

    Update messageHandler.js

--- inst/htmlwidgets/lib/messageHandler.js
+++ inst/htmlwidgets/lib/messageHandler.js
@@ -2,6 +2,8 @@ function img_clssfctn_msg_handler(){
     let imgSize ;
     Shiny.addCustomMessageHandler("img_clssfctn_ud_batch_image_size",
         function(message) {
+            console.log("MESSAGE VALUE : " + message );
+
             imgSize =  parseInt(JSON.stringify(message));
         });
         console.log("imgSize in handler  : " + imgSize );
