8ed090415d63f71cd33d8acc57e1476ce949b8e1
dmdeveloper <kudatawira@gmail.com>
2019-11-29 10:53:47 +0200

    Update imgClassification.js

--- inst/htmlwidgets/lib/imgClassification.js
+++ inst/htmlwidgets/lib/imgClassification.js
@@ -98,11 +98,13 @@
     /*Shiny.addCustomMessageHandler("img_clssfctn_ud_batch_image_size",
       function(message) {
         imgNumb =  parseInt(JSON.stringify(message));
-              Shiny.onInputChange("img_clssfctn_ud_btch_tckr",
-            1 + " / " + getBatchNumber());
+
         }
-    );*/    alert("ImageNumber : " + imgNumb);
-              initial(imgNumb,0);
+    );*/
+    alert("ImageNumber : " + imgNumb);
+    Shiny.onInputChange("img_clssfctn_ud_btch_tckr",
+            1 + " / " + getBatchNumber());
+    initial(imgNumb,0);
 
   }
 
