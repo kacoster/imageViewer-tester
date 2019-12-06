139e773c75d7c900f3236c3029c27cb3f74d9b05
dmdeveloper <kudatawira@gmail.com>
2019-12-06 10:28:58 +0200

    refector package img_clssfctn

---
+++ inst/htmlwidgets/viewerimgclssfctnud.js
@@ -0,0 +1,49 @@
+HTMLWidgets.widget({
+
+  name: 'viewerimgclssfctnud',
+
+  type: 'output',
+
+  factory: function(el, width, height) {
+
+    // TODO: define shared variables for this instance
+
+    return {
+
+      renderValue: function(x) {
+
+       console.log("case classification module ");
+
+           //readServerData(x.message);
+           Shiny.addCustomMessageHandler("img_clssfctn_ud_batch_image_size",
+              function(message) {
+                setImagesNumber(parseInt(JSON.stringify(message)));
+                console.log("Handler img_clssfctn_ud_batch_image_size " + parseInt(JSON.stringify(message)));
+                 //readServerData(x.message);
+                }
+            );
+
+            Shiny.addCustomMessageHandler("img_clssfctn_ud_img_clmn_numb",
+              function(message) {
+                setColumnNumb(parseInt(JSON.stringify(message)));
+                console.log("Handler img_clssfctn_ud_img_clmn_numb : " + parseInt(JSON.stringify(message)));
+                }
+            );
+
+            Shiny.addCustomMessageHandler("img_clssfctn_ud_fltr_button",
+                function(mesg) {
+                  console.log("Handler img_clssfctn_ud_fltr_button");
+                  readServerData(x.message);
+                }
+            );
+      },
+
+      resize: function(width, height) {
+
+        // TODO: code to re-render the widget with a new size
+
+      }
+
+    };
+  }
+});
