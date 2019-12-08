c93766f7b157f87e0341715c79693bb2f967e1b7
dmdeveloper <kudatawira@gmail.com>
2019-12-08 16:50:20 +0200

    Update viewerHelpers.js

--- inst/htmlwidgets/lib/viewerHelpers.js
+++ inst/htmlwidgets/lib/viewerHelpers.js
@@ -1,9 +1,9 @@
-var imgClssfctnObj = new ViewerComponent(0,50,5,"img_clssfctn_ud",csvfile);
+
 function fetchServerData(csvfile,moduleId)
 {
+    var imgClssfctnObj = new ViewerComponent(0,50,5,"img_clssfctn_ud",csvfile);
     if(moduleId === "img_clssfctn_ud")
     {
-        
         imgClssfctnObj.readServerData();
     }
 }
