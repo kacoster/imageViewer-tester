3524a8b971a338c346ca20f0f237ce09c6bde5f4
dmdeveloper <kudatawira@gmail.com>
2019-12-08 09:25:20 +0200

    Update viewerComponent.js

--- inst/htmlwidgets/lib/viewerComponent.js
+++ inst/htmlwidgets/lib/viewerComponent.js
@@ -33,21 +33,21 @@ class ViewerComponent {
     }
   
     processXHTTP(xhttp) {
-      
-      this.imgArray = (xhttp.responseText.replace(/^\s*$[\n\r]{1,}/gm, '')).split(',');
-      this.imgArray.splice(0, 1);
-      this.imgArray[0] = this.imgArray[0].replace("Source", "");
-      this.imgArray[0] = this.imgArray[this.imgArray.length - 1] + this.imgArray[0];
-      this.imgArray.splice(this.imgArray.length - 1, 1);
-     
-      console.log(this.imgArray);
+      let ar = [];
+      ar = (xhttp.responseText.replace(/^\s*$[\n\r]{1,}/gm, '')).split(',');
+      ar.splice(0, 1);
+      ar[0] = ar[0].replace("Source", "");
+      ar[0] = ar[ar.length - 1] + ar[0];
+      ar.splice(ar.length - 1, 1);
+      console.log(ar);
+      this.imgArray = [...ar];
+
       if(this.moduleId === "img_clssfctn_ud")
       {
         Shiny.onInputChange("img_clssfctn_ud_btch_tckr",
         1 + " / " + this.getBatchNumber());
       }
       this.callImges(this.displayImages(this.imgNumb,0));
-  
     }
 
     setImagesNumber(numb)
