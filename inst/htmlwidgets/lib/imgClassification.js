04b15abf0b17d4f8f3517eedb7802b6dd0d9cbd1
dmdeveloper <kudatawira@gmail.com>
2019-12-08 17:30:12 +0200

    Update imgClassification.js

--- inst/htmlwidgets/lib/imgClassification.js
+++ inst/htmlwidgets/lib/imgClassification.js
@@ -41,7 +41,7 @@
 
   });
 
-  var imgClssfctnObj = new ViewerComponent(0,50,5,"img_clssfctn_ud",csvfile);
+  var imgClssfctnObj = new ViewerComponent(0,50,5,"img_clssfctn_ud","img_clssfctn_ud.csv");
 
   /**
   * Handles all image panel click events
