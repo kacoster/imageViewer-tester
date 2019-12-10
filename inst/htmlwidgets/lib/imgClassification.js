1ff44675f62162c7328f66789723045af2452227
dmdeveloper <kudatawira@gmail.com>
2019-12-10 11:46:53 +0200

    Update imgClassification.js

--- inst/htmlwidgets/lib/imgClassification.js
+++ inst/htmlwidgets/lib/imgClassification.js
@@ -99,11 +99,12 @@
   function saveButtonListerner()
   {
     console.log("Save Click");
+    imgClssfctnObj.liWhiteBackground();
     imgClssfctnObj.deSelectAll();
     //imgClssfctnObj.selected_images.length = 0;
     imgClssfctnObj.getCurrClckdImg("clssfctn_slctd_img","");
     imgClssfctnObj.getCurrClckdImg("clssfctn_vw_curr_img","");
-    //imgClssfctnObj.liWhiteBackground();
+    //
 
   }
 
