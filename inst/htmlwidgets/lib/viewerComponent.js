594b37cab9e0e79c8c174f9b2e7c9e7426cd5c5b
dmdeveloper <kudatawira@gmail.com>
2019-12-11 06:02:19 +0200

    Update viewerComponent.js

--- inst/htmlwidgets/lib/viewerComponent.js
+++ inst/htmlwidgets/lib/viewerComponent.js
@@ -207,7 +207,7 @@ class ViewerComponent {
     // See if this indeed should ne var 
     vjs(elementID) {
         console.log("vjs : " + elementID);
-        var viewer = new Viewer(document.getElementById(elementID), {
+        var elementID = new Viewer(document.getElementById(elementID), {
             url: 'data-original',
             title: function (image) {
             return image.alt + ' (' + (this.index + 1) + '/' + this.length + ')';
