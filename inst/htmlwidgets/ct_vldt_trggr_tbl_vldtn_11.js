a498cf1f227430853c2caf474ec2e65bbea47880
dmdeveloper <kudatawira@gmail.com>
2019-12-12 12:39:23 +0200

    Update ct_vldt_trggr_tbl_vldtn_11.js

--- inst/htmlwidgets/ct_vldt_trggr_tbl_vldtn_11.js
+++ inst/htmlwidgets/ct_vldt_trggr_tbl_vldtn_11.js
@@ -14,8 +14,8 @@ HTMLWidgets.widget({
            console.log("ct_vldt_img_trggr_tbl_vldtn_11");
            console.log("x.message : " + x.message);
            console.log("x.componentID : " + x.componentID);
-          
-          Shiny.addCustomMessageHandler("ct_vldt_trggr_tbl_vldtn_11_button",
+           //ct_vldt_trggr_tbl_vldtn_9_button
+          Shiny.addCustomMessageHandler("ct_vldt_trggr_tbl_vldtn_9_button",
                 function(mesg) {
                   console.log("Handler ct_vldt_trggr_tbl_vldtn_11_button");
                   fetchServerData(x.message,x.componentID);
