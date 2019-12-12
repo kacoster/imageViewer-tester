b73cf7f0c93394333bb99f75c425427d25a08372
dmdeveloper <kudatawira@gmail.com>
2019-12-12 10:31:19 +0200

    remove objectOf()

--- inst/htmlwidgets/lib/ct_vldt_img_trggr_tbl_vldtn_10.js
+++ inst/htmlwidgets/lib/ct_vldt_img_trggr_tbl_vldtn_10.js
@@ -13,7 +13,7 @@ var ct_vldt_img_trggr_tbl_vldtn_10 = new ViewerComponent(0,30,5,"ct_vldt_img_trg
     ct_vldt_img_trggr_tbl_vldtn_10.columnSize = 5;
     ct_vldt_img_trggr_tbl_vldtn_10.imgNumb = 30;
     ct_vldt_img_trggr_tbl_vldtn_10.setCol();
-    objectOf("ct_vldt_img_trggr_tbl_vldtn_10");
+    //objectOf("ct_vldt_img_trggr_tbl_vldtn_10");
     ct_vldt_img_trggr_tbl_vldtn_10.callvjs(ct_vldt_img_trggr_tbl_vldtn_10.moduleId+"_divId");
   }
 
--- inst/htmlwidgets/lib/ct_vldt_img_trggr_tbl_vldtn_9.js
+++ inst/htmlwidgets/lib/ct_vldt_img_trggr_tbl_vldtn_9.js
@@ -12,7 +12,7 @@ var ct_vldt_img_trggr_tbl_vldtn_9 = new ViewerComponent(0,30,5,"ct_vldt_img_trgg
     ct_vldt_img_trggr_tbl_vldtn_9.columnSize = 5;
     ct_vldt_img_trggr_tbl_vldtn_9.imgNumb = 30;
     ct_vldt_img_trggr_tbl_vldtn_9.setCol();
-    objectOf("ct_vldt_img_trggr_tbl_vldtn_9");
+    //objectOf("ct_vldt_img_trggr_tbl_vldtn_9");
     ct_vldt_img_trggr_tbl_vldtn_9.callvjs(ct_vldt_img_trggr_tbl_vldtn_9.moduleId+"_divId");
   }
 
