492bfd3be77ec88e3628385f3360511c087c0248
dmdeveloper <kudatawira@gmail.com>
2019-12-31 13:34:12 +0200

    Update viewerimgclssfctnud.js

--- inst/htmlwidgets/viewerimgclssfctnud.js
+++ inst/htmlwidgets/viewerimgclssfctnud.js
@@ -36,6 +36,7 @@ HTMLWidgets.widget({
             Shiny.addCustomMessageHandler("img_clssfctn_ud_fltr_button",
                 function(mesg) {
                   console.log("Handler img_clssfctn_ud_fltr_button");
+                  nextPrevClicked("1");
                   fetchServerData(x.message,x.componentID);
                   resetSelectedImages();
                 }
