15df91b0f275a1d10a2e9c8be7f57d3b83cef7fd
dmdeveloper <kudatawira@gmail.com>
2020-01-01 12:30:23 +0200

    Update viewerComponent.js

--- inst/htmlwidgets/lib/viewerComponent.js
+++ inst/htmlwidgets/lib/viewerComponent.js
@@ -270,25 +270,12 @@ class ViewerComponent {
         let img = new Image();
         img.src = ((ar[i].trim()).replace(/['"]+/g, '')).replace(/(\r\n|\n|\r)/gm,"");
         console.log(img.src);
-        $.ajax({
-                url:img.src,
-                type:'HEAD',
-                error: function()
-                {
-                    //file not exists
-                },
-                success: function()
-                {
-                  img.alt = "Camera Trap";
-                  img.datamarked = 0;
-                  ul.innerHTML += '<li  ><img id="' + liId +"_"+this.moduleId+ '" data-original="' + img.src + '"                      marked="' + img.datamarked + '" src="' + img.src + '" alt="' + img.alt + '" /> </li>';
-                  this.setCol();
-                    //file exists
-                }
-        });
 
         //console.log(this.imageExists(""+ img.src +""));
-
+        img.alt = "Camera Trap";
+        img.datamarked = 0;
+        ul.innerHTML += '<li  ><img id="' + liId +"_"+this.moduleId+ '" data-original="' + img.src + '"  marked="' + img.datamarked + '" src="' + img.src + '" alt="' + img.alt + '" /> </li>';
+        this.setCol();
       }
       //$('#'+this.moduleId +'').html($(ul).attr('onmousedown="' + this.isKeyPressed(event) +'"' ));
     }
