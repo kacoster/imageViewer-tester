c9ee7c5d16e98b4ef37e629f063971445a757eef
dmdeveloper <kudatawira@gmail.com>
2020-01-07 10:26:15 +0200

    arlet image existance status

--- inst/htmlwidgets/lib/viewerComponent.js
+++ inst/htmlwidgets/lib/viewerComponent.js
@@ -283,12 +283,15 @@ class ViewerComponent {
         console.log("brokenImges");
           //$('#'+liId).hide();
       }*/
-      /*console.log("ar.length : " + ar.length);
+      console.log("ar.length : " + ar.length);
       if(this.checkImageExistance(ar) == ar.length)
       {
-        alert("No Images Found");
-      }*/
-     // else{
+         Shiny.setInputValue('no_srv_imgs', 'no imgs')
+         //alert("No Images Found");
+      }
+      else if(this.checkImageExistance(ar) > 0 || this.checkImageExistance(ar) < ar.length)
+      {
+        Shiny.setInputValue('mssng_srv_imgs', 'missing imgs');
         let ul = document.getElementById(this.moduleId);
         for (let i = 0; i < ar.length; i++) {
           let liId = i + this.moduleId;
@@ -302,7 +305,23 @@ class ViewerComponent {
           //document.getElementById(liId).addEventListener("error", brokenImges(liId));
           this.setCol();
        }
-     // }
+
+      }
+     else{
+        let ul = document.getElementById(this.moduleId);
+        for (let i = 0; i < ar.length; i++) {
+          let liId = i + this.moduleId;
+          let img = new Image();
+          img.src = ((ar[i].trim()).replace(/['"]+/g, '')).replace(/(\r\n|\n|\r)/gm,"");
+          //console.log(this.imageExists(""+ img.src +""));
+          //this.checkImageExistance(img.src)
+          img.alt = "Camera Trap";
+          img.datamarked = 0;
+          ul.innerHTML += '<li  ><img id="' + liId + '" data-original="' + img.src + '"  marked="' + img.datamarked + '" src="' + img.src + '"onerror="'+ "this.style.display='none'" +'"  alt="' + img.alt + '" /> </li>';
+          //document.getElementById(liId).addEventListener("error", brokenImges(liId));
+          this.setCol();
+        }
+      }
       
       //$('#'+this.moduleId +'').html($(ul).attr('onmousedown="' + this.isKeyPressed(event) +'"' ));
     }
