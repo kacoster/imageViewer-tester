48a78ba0ccf08ce64e304ae9651f2078c86effb3
dmdeveloper <kudatawira@gmail.com>
2020-01-28 12:36:09 +0200

    Update viewerimgclssfctnud.js

--- inst/htmlwidgets/viewerimgclssfctnud.js
+++ inst/htmlwidgets/viewerimgclssfctnud.js
@@ -38,7 +38,7 @@ HTMLWidgets.widget({
                   console.log("Handler img_clssfctn_ud_fltr_button");
                   nextPrevClicked("1");
                   fetchServerData(x.message,x.componentID);
-                  resetProps()();
+                  resetProps();
                 }
             );
 
